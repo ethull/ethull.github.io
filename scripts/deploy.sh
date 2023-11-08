a38f3815b3ac6a98f3dc91d3e5afd41e2241c133
Ethan Hullett <ethull@protonmail.com>
2023-11-08 09:27:52 +0000

    add scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #JEKYLL_ENV=production bundle exec jekyll serve --livereload 
-if [ $1 == "s" ]; then
+if [ $1 == "serve" ]; then
     bundle exec jekyll serve --livereload --config _config.yml,_dev_config.yml
-elif [ $1 == "b" ]; then
+elif [ $1 == "build" ]; then
     bundle exec jekyll build --config _config.yml,_dev_config.yml
-elif [ $1 == "ps" ]; then
+elif [ $1 == "prod-server" ]; then
     bundle exec jekyll serve --livereload
-elif [ $1 == "pb" ]; then
+elif [ $1 == "prod-build" ]; then
     bundle exec jekyll build
 fi
---
+++ scripts/setup_webfonts.sh
@@ -0,0 +1,2 @@
+# jekyll sass only imports sass files
+ln -s node_modules/@fortawesome/fontawesome-free/webfonts/ assets/webfonts
