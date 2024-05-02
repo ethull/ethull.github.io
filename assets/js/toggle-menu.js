4654e9356b78119627c379ba14f024797c15ac2f
ethull <ethull@protonmail.com>
2024-05-02 16:03:16 +0100

    code formatting

--- assets/js/toggle-menu.js
+++ assets/js/toggle-menu.js
@@ -2,5 +2,5 @@ const menu = document.querySelector('#navMenubd-main');
 
 document.querySelector(".navbar-burger").addEventListener("click", (e) => {
   e.preventDefault();
-  menu.classList.toggle('is-active');
+  menu.classList.toggle("is-active");
 });
