f0a036404aaafa6c066ac12d30cbed20f752baf5
ethull <ethull@protonmail.com>
2024-05-02 16:45:21 +0100

    update scripts

--- scripts/date_gen.sh
+++ scripts/date_gen.sh
@@ -1,2 +1,4 @@
 # gen current date in ISO-8601 format
 date -Isec
+# gen display date
+date +%Y-%m-%d
--- scripts/setup_webfonts.sh
+++ scripts/setup_webfonts.sh
@@ -1,2 +1,2 @@
 # jekyll sass only imports sass files
-ln -s node_modules/@fortawesome/fontawesome-free/webfonts/ assets/webfonts
+ln -s ../node_modules/@fortawesome/fontawesome-free/webfonts/ assets/webfonts
